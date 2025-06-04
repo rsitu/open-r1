24d778c701e419d73d75b63d0d87c6efe87af66c
rsitu <damonsitu@gmail.com>
2025-06-04 07:24:53 +0000

    exploring repo

--- src/open_r1/sft_toy.py
+++ src/open_r1/sft_toy.py
@@ -23,24 +23,52 @@ def main(script_args, training_args, model_args):
     ###############
     # Setup logging
     ###############
+    print("\n===logging setup===\n")
     logging.basicConfig(
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
         handlers=[logging.StreamHandler(sys.stdout)],
     )
     log_level = training_args.get_process_log_level()
-    log_level = 20
-    print(f"log_level: {log_level}")
-
     logger.setLevel(log_level)
     datasets.utils.logging.set_verbosity(log_level)
     transformers.utils.logging.set_verbosity(log_level)
     transformers.utils.logging.enable_default_handler()
     transformers.utils.logging.enable_explicit_format()
 
+    print(f"log_level: {log_level}")
     logger.info(f"Model parameters {model_args}\n")
     logger.info(f"Script parameters {script_args}\n")
     logger.info(f"Training parameters {training_args}\n")
+
+    print("\n===last checkpoint args===\n")
+
+    # Check for last checkpoint
+    last_checkpoint = None
+    if os.path.isdir(training_args.output_dir):
+        print(f"training_args.output_dir: {training_args.output_dir}")
+        last_checkpoint = get_last_checkpoint(training_args.output_dir)
+
+    if last_checkpoint is not None and training_args.resume_from_checkpoint is None:
+        print(f"training_args.resume_from_checkpoint: {training_args.resume_from_checkpoint}")
+        logger.info(f"Checkpoint detected, resuming training at {last_checkpoint=}.")
+
+    if "wandb" in training_args.report_to:
+        print(f"training_args.report_to: {training_args.report_to}")
+        init_wandb_training(training_args)
+      
+    ######################################
+    # Load dataset, tokenizer, and model #
+    ######################################
+    print("\n===starting getting datasets===\n")
+    dataset = get_dataset(script_args)
+
+    # tokenizer = get_tokenizer(model_args, training_args)
+    # model = get_model(model_args, training_args)
+
+    # if tokenizer.chat_template is None:
+    #     logger.info("No chat template provided, defaulting to ChatML.")
+    #     model, tokenizer = setup_chat_format(model, tokenizer, format="chatml")
 
 if __name__ == "__main__":
     print("\n===starting of __main__===\n")
